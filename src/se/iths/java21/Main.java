968854f926bfb12da4afca25273605c6b57156f0
Lukas <lukas.ikvm1@gmail.com>
2021-08-19 23:35:29 +0200

    Javaiths main

---
+++ src/se/iths/java21/Main.java
@@ -0,0 +1,17 @@
+package se.iths.java21;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.console().readPassword();
+
+        System.out.println("hej");
+
+
+        //Hello
+
+        System.out.println("hej");
+        System.out.println(1+1);
+
+    }
+}

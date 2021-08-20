378e129ec310b6fd228a0a4e72a932b08ce3d895
Lukas <lukas.ikvm1@gmail.com>
2021-08-20 13:42:46 +0200

    Javaiths main

--- src/se/iths/java21/Main.java
+++ src/se/iths/java21/Main.java
@@ -1,19 +1,53 @@
 package se.iths.java21;
 
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.console().readPassword();
 
-        System.out.println("hej");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Guess the number!, betwenn 1-100");
 
 
-        //Hello
 
-        System.out.println("hej");
-        System.out.println(1+1);
+        int hemligt = (int) (Math.random()*101);
+        int guess = 0;
+        ArrayList<Integer> list = new ArrayList<Integer>();
 
-        System.out.println("Pog");
+        while(guess != hemligt){
+             guess = sc.nextInt();
+             if(guess > hemligt){
+                 System.out.println("A little too high!");
+                 list.add(guess);
+
+             }else if(guess == hemligt){
+                 System.out.println("Correct!");
+
+                 System.out.println("Here are you guesses!");
+                 for(int x : list){
+
+                     System.out.printf( list.indexOf(x) + 1 + ". Guess was %d \n",x);
+                 }
+             }
+             else{
+                 System.out.println("A little too low");
+                 list.add(guess);
+             }
+
+
+        }
+
+
+
+
+
+
+
+
+
 
     }
 }

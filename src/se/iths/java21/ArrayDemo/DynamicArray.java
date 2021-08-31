f7fa42a02b70e39944cf7c18fb1efb0d9661a353
Lukas <lukas.ikvm1@gmail.com>
2021-08-31 11:24:56 +0200

    DynamicArray

--- src/se/iths/java21/ArrayDemo/DynamicArray.java
+++ src/se/iths/java21/ArrayDemo/DynamicArray.java
@@ -3,13 +3,33 @@ package se.iths.java21.ArrayDemo;
 public class DynamicArray {
 
     int[] values = new int[10];
+    int counter = 0;
 
     public static void main(String[] args) {
+
         DynamicArray dynamicArray = new DynamicArray();
+
+        dynamicArray.add(1);
+        dynamicArray.add(2);
+        for (int value : dynamicArray.values) {
+            System.out.println(value);
+        }
+
     }
 
     void add(int value){
+        values[counter] = value;
+        counter = counter + 1;
+    }
 
+    int removeLast(){//Börja skriva ett test först sen själva koden, TDD = test driven development
+        if(counter == 0)
+            return 0;
+        return values[--counter];
+    }
+
+    int length(){
+        return counter;
     }
 
 }

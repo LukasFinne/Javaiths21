4fc9ddd60f6b7f8c025c2fb52f44467109c06f34
Lukas <lukas.ikvm1@gmail.com>
2021-09-20 11:47:47 +0200

    List interfaces

---
+++ src/se/iths/java21/collections/Demo.java
@@ -0,0 +1,12 @@
+package se.iths.java21.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("First");
+    }
+}

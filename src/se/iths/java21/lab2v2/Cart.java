685fd638bb347c8c092d32eda679a5de6c2adfbf
Lukas <lukas.ikvm1@gmail.com>
2021-09-30 19:32:06 +0200

    Added if you buy 2 of the same items it will increase the price, next is to remove the second item from the cart

--- src/se/iths/java21/lab2v2/Cart.java
+++ src/se/iths/java21/lab2v2/Cart.java
@@ -1,8 +1,7 @@
 package se.iths.java21.lab2v2;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Cart implements Command{
@@ -13,14 +12,21 @@ public class Cart implements Command{
 
     public void showCart(){
         System.out.println("Products in the cart:");
-        cart.stream().flatMap(Collection::stream).forEach(System.out::println);
-
+        cart.forEach(System.out::println);
+        System.out.println(checkFrequency(testList(cart)));
     }
 
 
     public void addToCart(List<NameAndPrice> list){
         productListCopy = new ArrayList<>(list);
         cart.add(productListCopy);
+
+        if(checkFrequency(testList(cart)) > 1){
+            cart.stream()
+                    .flatMap(List::stream)
+                    .toList().get(0).increasePrice();
+        }
+
         realItemCheck();
     }
 
@@ -29,6 +35,18 @@ public class Cart implements Command{
             System.out.println("This is item does not exist, please try again");
         } else
             System.out.println("Added to cart");
+    }
+
+    private List<NameAndPrice> testList(List<List<NameAndPrice>> list){
+        return cart.stream()
+                .flatMap(List::stream)
+                .toList();
+    }
+
+    private int checkFrequency(List<NameAndPrice> list){
+        String productsCheck = productListCopy.get(0).productName();
+        List<String> test = list.stream().map(NameAndPrice::productName).toList();
+        return Collections.frequency(test, productsCheck);
     }
 
     @Override
--- src/se/iths/java21/lab2v2/NameAndPrice.java
+++ src/se/iths/java21/lab2v2/NameAndPrice.java
@@ -1,8 +1,47 @@
 package se.iths.java21.lab2v2;
 
-record NameAndPrice(String productName, int price) {
+import javax.naming.Name;
+import java.util.Objects;
+
+ public class NameAndPrice {
+    private final String productName;
+    private int price;
+
+
+    public NameAndPrice(String productName, int price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
     @Override
     public String toString() {
         return productName + ", " + price + "Kr";
     }
+
+    public String productName() {
+        return productName;
+    }
+
+    public int price() {
+        return price;
+    }
+
+    public int increasePrice(){
+        return price += price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (NameAndPrice) obj;
+        return Objects.equals(this.productName, that.productName) &&
+                this.price == that.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productName, price);
+    }
+
 }

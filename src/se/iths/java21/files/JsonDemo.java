ee0459265216f0de4d083aeab8c3bebfaa40270a
Lukas <lukas.ikvm1@gmail.com>
2021-10-04 14:58:13 +0200

    Java to json,

--- src/se/iths/java21/files/JsonDemo.java
+++ src/se/iths/java21/files/JsonDemo.java
@@ -2,12 +2,16 @@ package se.iths.java21.files;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class JsonDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<Cake> cake = List.of(
                 new Cake(1, "KanelBulle", 20),
                 new Cake(2, "KanelBullea", 15),
@@ -16,6 +20,11 @@ public class JsonDemo {
         JsonDemo jsonDemo = new JsonDemo();
         String json = jsonDemo.toJson(cake);
         System.out.println(json);
+
+        String homeFolder = System.getProperty("user.home");
+        Path path = Path.of(homeFolder, "test", "cakes.json");
+        Files.writeString(path, json);
+
 
         List<Cake> jsonCakesList = jsonDemo.fromJson(json);
         jsonCakesList.forEach(System.out::println);
@@ -35,6 +44,12 @@ public class JsonDemo {
     }
 
     private List<Cake> fromJson(String jsonData){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(jsonData, new TypeReference<>(){});//TypeRefernce är för att type erasure
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
         return List.of();
     }
 }

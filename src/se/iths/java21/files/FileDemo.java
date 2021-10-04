9ac273406ac90902c1b359240757f11c05886c3f
Lukas <lukas.ikvm1@gmail.com>
2021-10-04 10:44:19 +0200

    File demo

--- src/se/iths/java21/files/FileDemo.java
+++ src/se/iths/java21/files/FileDemo.java
@@ -1,12 +1,36 @@
 package se.iths.java21.files;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class FileDemo {
 
-    public static void main(String[] args) {
-        Path path = Path.of("~", "test", "fil1.txt" );
+    public static void main(String[] args) throws IOException {
+        String homeFolder = System.getProperty("user.home");
+        Path path = Path.of(homeFolder, "test", "fil1.txt");
+        Files.createDirectories(path.getParent());
         System.out.println(Files.exists(path));
+        System.out.println(path.toAbsolutePath());
+
+
+        List<String> strings = List.of("First Line", "Second", "Third");
+        Files.write(path, strings, StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+
+        System.out.println(Files.exists(path));
+
+        //String s = Files.readString(path);
+        //System.out.println( s);
+        //List<String> stringList = Files.readAllLines(path);
+        // stringList.forEach(System.out::println);
+
+        Stream<String> stream = Files.lines(path);
+        stream.filter(i -> i.startsWith("F"))
+                .forEach(System.out::println);
+
     }
 }

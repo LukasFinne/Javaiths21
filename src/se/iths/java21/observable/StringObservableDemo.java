bb4659deb5143ac0b74404100d5e2a98a3cd1803
Lukas <lukas.ikvm1@gmail.com>
2021-10-12 15:01:30 +0200

    Adds Observable implmentation

---
+++ src/se/iths/java21/observable/StringObservableDemo.java
@@ -0,0 +1,19 @@
+package se.iths.java21.observable;
+
+public class StringObservableDemo {
+    public static void main(String[] args) {
+        StringObservable stringObservable = new StringObservable();
+
+        Observer observer = () -> System.out.println("Value has changed: " + stringObservable.getValue());
+
+        stringObservable.register(observer);
+
+        stringObservable.setValue("Hej");
+        stringObservable.setValue("Då!");
+        stringObservable.setValue("Då!");
+
+        stringObservable.unregister(observer);
+
+        stringObservable.setValue("Trevlig kväll!");
+    }
+}

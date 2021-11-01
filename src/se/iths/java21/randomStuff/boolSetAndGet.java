f542d128ed24bb57a165281711e5a6f702e09b80
Lukas <lukas.ikvm1@gmail.com>
2021-11-01 21:56:08 +0100

    Just random things for Java test

--- src/se/iths/java21/randomStuff/boolSetAndGet.java
+++ src/se/iths/java21/randomStuff/boolSetAndGet.java
@@ -1,7 +1,19 @@
 package se.iths.java21.randomStuff;
 
-public class boolSetAndGet {
+public class boolSetAndGet implements b{
 
+
+    public static void main(String[] args) {
+        a test = new boolSetAndGet();
+        String s = "";
+        int a = 1;
+
+        s += a;
+
+        System.out.println(s);
+
+
+    }
 
     private boolean b;
 
@@ -12,5 +24,13 @@ public class boolSetAndGet {
     public void setB(boolean b) {
         this.b = b;
     }
+
+}
+
+interface a {
+
+}
+
+interface b extends a {
 
 }
